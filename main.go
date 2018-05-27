163537cab0b5d8127e85b07748f7aa989843297e
Camal Cakar <camal_cakar@yahoo.de>
2018-05-27 21:51:26 +0200

    Concurrent workflow implementation

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	customerOrders := []string{"cappuccino", "flat white", "americano", "americano"}
+
+	brewChan := make(chan string, 1)
+	distributeChan := make(chan string, 2)
+	doneChan := make(chan bool)
+
+	go func(orders []string) {
+		for _, order := range orders {
+			fmt.Println("Received new order: ", order)
+			brewChan <- order
+		}
+
+		close(brewChan)
+	}(customerOrders)
+
+	go func() {
+		for order := range brewChan {
+			fmt.Println("Barista is preparing: ", order)
+
+			if order == "flat white" {
+				time.Sleep(3 * time.Second)
+			}
+
+			distributeChan <- order
+		}
+
+		close(distributeChan)
+	}()
+
+	go func() {
+		for coffee := range distributeChan {
+			fmt.Println("Happy customer with a ", coffee)
+		}
+
+		close(doneChan)
+	}()
+
+	<- doneChan
+}
